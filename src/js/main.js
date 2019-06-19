6f1c8c4b79fc06d47031b499062ca764362cbbda
krijnvdker <krijnvdker@gmail.com>
2019-06-19 23:11:54 +0200

    finished menu

--- src/js/main.js
+++ src/js/main.js
@@ -3,19 +3,14 @@ $(document).ready(function(){
         $(".mobile-nav").addClass('open');
         $(".main").addClass('open');
     });
-    // $('.mobile-nav').click(function(){
-    //     console.log('tewofjoiwejf');
-    //     $(this).removeClass('open');
-    // });
     $('.close-menu').click(function(){
-        // console.log('tewofjoiwejf');
         $('.mobile-nav').removeClass('open');
         $('.main').removeClass('open');
     });
 
-    $('.has-sub').click(function(){
+    $('.has-sub').click(function(e){
+        e.preventDefault();
         $('.has-sub ul').toggleClass('open');
-        //$('.has-sub ul').animate({"height": "300px"});
         $('.fa-plus').toggle();
         $('.fa-minus').toggle();
     });
