dba4c25b863e0b68e802e3b137fd5fe0ae00e97e
Ricardo Agra <agra.ric@gmail.com>
2020-01-15 16:24:10 +0100

    added homepage blocks

--- src/js/main.js
+++ src/js/main.js
@@ -2,5 +2,10 @@ $(document).ready(function(){
     $(".hamburger-icon").click(function(){
         $(".menu, .menu-overlay, .hamburger-icon").addClass('open');
     });
+    $(".portfolio-item").hover(function(){
+            $(this).find(".overlay").show();
+        }, function(){
+            $(this).find(".overlay").hide();
+    });
 
 });
